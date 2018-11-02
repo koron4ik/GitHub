9a15990a7a88a79de9fe95daa59589b8c13ed015
Vadim Koronchik <koronchik@icloud.com>
2018-11-02 23:45:23 +0300

    Created EnterViewController

---
+++ GitHub/EnterViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by Vadim on 11/2/18.
+//  Copyright © 2018 Koronchik. All rights reserved.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+
+    lazy var githubLogoImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: (self.view.frame.size.width - 130) / 2, y: (self.view.frame.size.height - 300) / 2, width: 130, height: 130))
+        
+        imageView.image = UIImage(named: "github_logo")
+        
+        return imageView
+    }()
+    
+    lazy var githubTextImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: (self.view.frame.size.width - 300) / 2, y: (self.view.frame.size.height) / 2, width: 300, height: 100))
+        
+        imageView.image = UIImage(named: "github_text_logo")
+        
+        return imageView
+    }()
+    
+    
+    lazy var signInButton: UIButton = {
+        let button = UIButton()
+        
+        button.frame = CGRect(x: (self.view.frame.size.width - 300) / 2, y: self.view.frame.size.height - 100, width: 300, height: 50)
+        button.setImage(UIImage(named: "login"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action:#selector(buttonClicked), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationController?.isNavigationBarHidden = true
+        
+        self.view.addSubview(signInButton)
+        self.view.addSubview(githubTextImageView)
+        self.view.addSubview(githubLogoImageView)
+    }
+
+    @objc func buttonClicked() {
+        print("Clicked")
+    }
+}
+
