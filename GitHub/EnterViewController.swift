dc159f5fd3618ac6111685f128887ef4c5e9798d
Vadim Koronchik <koronchik@icloud.com>
2018-11-03 00:36:07 +0300

    Added animations

--- GitHub/EnterViewController.swift
+++ GitHub/EnterViewController.swift
@@ -14,6 +14,7 @@ class EnterViewController: UIViewController {
         let imageView = UIImageView(frame: CGRect(x: (self.view.frame.size.width - 130) / 2, y: (self.view.frame.size.height - 300) / 2, width: 130, height: 130))
         
         imageView.image = UIImage(named: "github_logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
@@ -22,6 +23,7 @@ class EnterViewController: UIViewController {
         let imageView = UIImageView(frame: CGRect(x: (self.view.frame.size.width - 300) / 2, y: (self.view.frame.size.height) / 2, width: 300, height: 100))
         
         imageView.image = UIImage(named: "github_text_logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
@@ -48,9 +50,57 @@ class EnterViewController: UIViewController {
         self.view.addSubview(githubTextImageView)
         self.view.addSubview(githubLogoImageView)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        beginButtonAnimation()
+    }
 
     @objc func buttonClicked() {
         print("Clicked")
+        animationButtonPressed()
+    }
+        
+    func startRepeatingButtonAnimation() {
+        signInButton.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        UIView.animate(withDuration: 1.0,
+            delay: 0.0,
+            options: [.repeat, .autoreverse, .allowUserInteraction],
+            animations: {
+                self.signInButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }
+        )
+    }
+    
+    func beginButtonAnimation() {
+        signInButton.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+        UIView.animate(withDuration: 2.0,
+            delay: 0.2,
+            usingSpringWithDamping: 1.0,
+            initialSpringVelocity: 1.0,
+            animations: {
+                self.signInButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.startRepeatingButtonAnimation()
+            }
+        )
+    }
+    
+    func animationButtonPressed() {
+        signInButton.layer.removeAllAnimations()
+        UIView.animate(withDuration: 0.2,
+            delay: 0.1,
+            usingSpringWithDamping: 1.5,
+            initialSpringVelocity: 0.0,
+            animations: {
+                self.signInButton.transform = CGAffineTransform(scaleX: 1.1, y: 1.2)
+            },
+            completion: { _ in
+                UIView.animate(withDuration: 0.2) {
+                    self.signInButton.transform = CGAffineTransform.identity
+                }
+            }
+        )
     }
 }
 
